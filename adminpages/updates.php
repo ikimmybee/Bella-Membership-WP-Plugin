64cf386c1dcaa68a2890280c68aa5b314be0fbfe
Kimmy GG <ikimmybee@users.noreply.github.com>
2017-03-18 16:37:49 -0700

    Update updates.php

--- adminpages/updates.php
+++ adminpages/updates.php
@@ -2,7 +2,7 @@
 	//only admins can get this
 	if(!function_exists("current_user_can") || (!current_user_can("manage_options") && !current_user_can("pmpro_updates")))
 	{
-		die(__("You do not have permissions to perform this action.", 'paid-memberships-pro' ));
+		die(__("You do not have permissions to perform this action.", 'bella-membership-plugin' ));
 	}
 
 	//reset this transient so we know the page was just loaded
@@ -11,14 +11,14 @@
 	require_once(dirname(__FILE__) . "/admin_header.php");	
 ?>
 
-<h2><?php _e('Updating Paid Memberships Pro', 'paid-memberships-pro' );?></h2>
+<h2><?php _e('Updating Bella Membership Plugin', 'bella-membership-plugin' );?></h2>
 
 <?php
 	$updates = get_option('pmpro_updates', array());
 	if(!empty($updates)) {
 		//let's process the first one
 	?>
-	<p id="pmpro_updates_intro"><?php _e('Updates are processing. This may take a few minutes to complete.', 'paid-memberships-pro' );?></p>
+	<p id="pmpro_updates_intro"><?php _e('Updating.. Please wait.', 'bella-membership-plugin' );?></p>
 	<p id="pmpro_updates_progress">[...]</p>
 	<textarea id="pmpro_updates_status" rows="10" cols="60">Loading...</textarea>
 	
