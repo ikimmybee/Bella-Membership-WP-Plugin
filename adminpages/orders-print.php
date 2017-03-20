aa6c2f58a510802863b8cd4f2d0822e0c251f910
Kimmy GG <ikimmybee@users.noreply.github.com>
2017-03-20 15:30:59 -0700

    Update orders-print.php

--- adminpages/orders-print.php
+++ adminpages/orders-print.php
@@ -4,12 +4,12 @@
  *
  * Gets the order and displays the print view template.
  *
- * @since 1.8.6
+ * @since 1.0
  */
 
 //only admins can get this
 if ( ! function_exists( "current_user_can" ) || ( ! current_user_can( "manage_options" ) && ! current_user_can( "pmpro_ordersprint" ) ) ) {
-	die( __( "You do not have permissions to perform this action.", 'paid-memberships-pro' ) );
+	die( __( "You do not have permissions to perform this action.", 'bella-membership-plugin' ) );
 }
 
 // Do we have an order ID?
@@ -23,10 +23,10 @@ $order = new MemberOrder($_REQUEST['order']);
 $level = pmpro_getLevel($order->membership_id);
 
 // Load template
-if ( file_exists( get_stylesheet_directory() . '/paid-memberships-pro/pages/orders-print.php' ) ) {
-	$template = get_stylesheet_directory() . '/paid-memberships-pro/pages/orders-print.php';
-} elseif ( file_exists( get_template_directory() . '/paid-memberships-pro/pages/orders-print.php' ) ) {
-	$template = get_template_directory() . '/paid-memberships-pro/pages/orders-print.php';
+if ( file_exists( get_stylesheet_directory() . '/bella-membership-plugin/pages/orders-print.php' ) ) {
+	$template = get_stylesheet_directory() . '/bella-membership-plugin/pages/orders-print.php';
+} elseif ( file_exists( get_template_directory() . '/bella-membership-plugin/pages/orders-print.php' ) ) {
+	$template = get_template_directory() . '/bella-membership-plugin/pages/orders-print.php';
 } else {
 	$template = PMPRO_DIR . '/adminpages/templates/orders-print.php';
 }
