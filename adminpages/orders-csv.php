e8ae2b28937739d2fe1c564b30fc815e397ca231
Kimmy GG <ikimmybee@users.noreply.github.com>
2017-03-20 14:41:52 -0700

    Update orders-csv.php

--- adminpages/orders-csv.php
+++ adminpages/orders-csv.php
@@ -1,7 +1,7 @@
 <?php
 //only admins can get this
 if ( ! function_exists( "current_user_can" ) || ( ! current_user_can( "manage_options" ) && ! current_user_can( "pmpro_orderscsv" ) ) ) {
-	die( __( "You do not have permissions to perform this action.", 'paid-memberships-pro' ) );
+	die( __( "You do not have permissions to perform this action.", 'bella-membership-plugin' ) );
 }
 
 define('PMPRO_BENCHMARK', true);
@@ -536,7 +536,7 @@ function pmpro_transmit_order_content( $csv_fh, $filename, $headers = array() ) 
 	if ( headers_sent() ) {
 		echo str_repeat( '-', 75 ) . "<br/>\n";
 		echo 'Please open a support case and paste in the warnings/errors you see above this text to\n ';
-		echo 'the <a href="http://paidmembershipspro.com/support/" target="_blank">Paid Memberships Pro support forum</a><br/>\n';
+		echo 'the <a href="http://millennialbella.com/support/" target="_blank">Millennial Bella Support Forum</a><br/>\n';
 		echo str_repeat( "=", 75 ) . "<br/>\n";
 		echo file_get_contents( $filename );
 		echo str_repeat( "=", 75 ) . "<br/>\n";
